586a5538089e161d0a6442fe717e06fcb5b1b99d
afkus3r <sheila.skalli@gmail.com>
2023-01-22 17:28:57 +0100

    Initialize spring boot project

---
+++ src/main/java/shs/sheilaJava/polum/PolumApplication.java
@@ -0,0 +1,13 @@
+package shs.sheilaJava.polum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shs/sheilaJava/polum/PolumApplicationTests.java
@@ -0,0 +1,13 @@
+package shs.sheilaJava.polum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
