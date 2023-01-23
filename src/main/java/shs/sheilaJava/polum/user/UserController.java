6a705d074a100300e733dab7ac839a9ad9e73e73
afkus3r <sheila.skalli@gmail.com>
2023-01-23 16:07:07 +0100

    Create CRUD Users

--- src/main/java/shs/sheilaJava/polum/user/UserController.java
+++ src/main/java/shs/sheilaJava/polum/user/UserController.java
@@ -1,19 +1,32 @@
 package shs.sheilaJava.polum.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserRepository userRepository;
     @GetMapping
     public List<User> getAllUsers() {
-        return List.of(
-                new User(1, "user1", "user1@gmail.com", "123456"),
-                new User(2, "user2", "user2@gmail.com", "123456")
-        );
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+       return userRepository.save(user);
+    }
+
+    @PutMapping
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @DeleteMapping
+    public void deleteUser(Integer id) {
+        userRepository.deleteById(id);
     }
 }
