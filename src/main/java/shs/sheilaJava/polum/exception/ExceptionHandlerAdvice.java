437932d5a9505ba57a6da07d1ca2978a84126be2
afkus3r <sheila.skalli@gmail.com>
2023-02-04 19:54:24 +0100

    Create customized exception classes

---
+++ src/main/java/shs/sheilaJava/polum/exception/ExceptionHandlerAdvice.java
@@ -0,0 +1,20 @@
+package shs.sheilaJava.polum.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerAdvice {
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<Object> handleValidationException(ValidationException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
---
+++ src/main/java/shs/sheilaJava/polum/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package shs.sheilaJava.polum.exception;
+
+public class ResourceNotFoundException extends Exception{
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
